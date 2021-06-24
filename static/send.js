6b912795ee428e660695851ee6db2d4bc335c7cf
Arghya721 <60299552+Arghya721@users.noreply.github.com>
2021-06-24 23:46:43 +0530

    Update send.js

--- static/send.js
+++ static/send.js
@@ -31,7 +31,7 @@ function onFunc(){
 function offFunc(){
 
     http.put(
-        'http://localhost:5000/1',
+        'https://iotlightcontrol.herokuapp.com/1',
            off, function(err, post){
           if(err) {
             console.log(err);
